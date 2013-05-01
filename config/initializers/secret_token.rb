fa70c654bdd348d33b90dcece14d063174ccb95a
Collin Glass <collinglass@gmx.com>
2013-05-01 18:22:32 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberTry::Application.config.secret_token = '5abd6eab18ac64ec6a0af75c9e155651e1f9abf917971957011e10cfee65565ea2894a157c54cd65c67254b894de61425a79d3658d5860065f2c7d05723b3a3d'
